27446d7b8e61635d37568f9b91dd1f6a0b2ad7d1
MUKESHMUKESHMUKESH <meenakshivalarmathi@gmail.com>
2025-07-16 01:26:08 +0530

    change in appjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-
-import './App.css'
-import Nav  from './Components/Nav/Nav'
-import Header from './Components/Header/Header'
-import About from './Components/About/About'
-import Service from './Components/Service/Service'
-import Project from './Components/Project/project'
-import Education from './Components/Education/Education'
-import Number from './Components/Number/Number'
-
-
+import './App.css';
+import Nav from './Components/Nav/Nav';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Service from './Components/Service/Service';
+import Project from './Components/Project/Project';
+import Education from './Components/Education/Education';
+import Number from './Components/Number/Number';
 
 function App() {
-
-
   return (
     <>
       <Nav />
@@ -23,7 +18,7 @@ function App() {
       <Project />
       <Number />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
